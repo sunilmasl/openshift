42c2ed61e8f755a163e43f9395de57ecba19a03a
Sunil Kumar <36371126+sunilmasl@users.noreply.github.com>
2020-04-12 13:31:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello Sunil Say Hello to  OpenShift 4 </p>'; ?> 
+ <?php echo '<p>Hello Friends Say Hello to  OpenShift 4 </p>'; ?> 
  </body>
 </html>
